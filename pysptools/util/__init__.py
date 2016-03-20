3c1d562a9112083a846b5a614ec42e15df8c9df3
ctherien <christian.therien@gmail.com>
2016-03-20 11:21:46 -0400

    Create __init__.py

---
+++ pysptools/util/__init__.py
@@ -0,0 +1,12 @@
+"""
+"""
+
+try:
+    from .envi import load_ENVI_file, load_ENVI_spec_lib
+except ImportError:
+    pass
+from .fns import corr, cov
+from .display import plot_linear_stretch, display_linear_stretch
+from .input_vld import InputValidation, simple_decorator
+from .rois import ROIs
+from .data_format import convert2d, convert3d, normalize

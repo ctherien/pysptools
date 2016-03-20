6c365eecc110dbd012b9f5a0b1bf6e86df199768
ctherien <christian.therien@gmail.com>
2016-03-20 11:19:33 -0400

    Create __init__.py

---
+++ pysptools/spectro/__init__.py
@@ -0,0 +1,6 @@
+"""
+"""
+
+from .qhull import SpectrumConvexHullQuotient, FeaturesConvexHullQuotient
+from .hull_removal import convex_hull_removal
+from .speclib import USGS06SpecLib, SpecLib, EnviReader, JSONReader
